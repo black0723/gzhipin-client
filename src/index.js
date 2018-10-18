2129a1c698f22c5cdd27a1e42f2d26a623119f8d
diaogbl <djjgbl@cnki.net>
2018-10-18 17:24:41 +0800

    初始化react项目

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/*
+入口js
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Button} from 'antd-mobile'
+import {HashRouter,Route} from 'react-router-dom'
+
+
+ReactDOM.render(<Button type="primary">Test</Button>,document.getElementById('root'))
